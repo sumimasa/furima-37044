533a5ac5c78bfdc9aa9a2d5f4f4ce9529c2666d6
sumimasa <smngmsk1234@icloud.com>
2021-10-21 20:37:25 +0900

    商品一覧表示機能編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  def index    
-    @items=Item.order("created_at DESC").includes(:user)
-  
-    
-    
+  def index
+    @items = Item.order('created_at DESC').includes(:user)
   end
 
   def new
@@ -13,7 +10,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-   
+
     if @item.save
       redirect_to root_path
     else
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :send_standard
   has_one_attached :image
-  
 
   with_options presence: true do
     validates :image
@@ -19,9 +18,7 @@ class Item < ApplicationRecord
     validates :cost_allocation_id, numericality: { other_than: 0, message: '配送料の負担者ーを選択してください' }
     validates :area_id, numericality: { other_than: 0, message: '発送元の地域を選択してください' }
     validates :send_standard_id, numericality: { other_than: 0, message: '発送までの日数を選択してください' }
-    validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-      message: '価格は300円~9,999,999円、半角で入力してください' }
-    
-              
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                      message: '価格は300円~9,999,999円、半角で入力してください' }
   end
 end
