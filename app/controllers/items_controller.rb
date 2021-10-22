aeec84bd14534ce9fc189375e7cee1e7cb3f8b09
sumimasa <smngmsk1234@icloud.com>
2021-10-22 11:45:30 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :item_params_id, only: [:show, :edit]
+  before_action :move_index, only: [:edit]
   def index
     @items = Item.order('created_at DESC').includes(:user)
   end
@@ -19,7 +21,19 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      @item = Item.find(params[:id])
+      render :edit
+    end
   end
 
   private
@@ -27,5 +41,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :cost_allocation_id, :area_id, :send_standard_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def item_params_id
+    @item = Item.find(params[:id])
+  end
+
+  def move_index
+    @item = Item.find(params[:id])
+    redirect_to root_path if @item.user_id != current_user.id
   end
 end
