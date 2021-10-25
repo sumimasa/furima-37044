4f76d6caae07631adbac0731b7535e6f7b415cc0
sumimasa <smngmsk1234@icloud.com>
2021-10-25 11:11:36 +0900

    商品購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,44 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_params_id
+  before_action :move_index
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :area_id, :city, :house_number, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def item_params_id
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_index
+    redirect_to root_path if @item.id == current_user.id || !@item.order.nil?
+  end
+end
