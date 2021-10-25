7eade276883aa1e3b5a9325baf873bcb448527cc
sumimasa <smngmsk1234@icloud.com>
2021-10-25 11:39:46 +0900

    tokenの削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id, token: token)
+    order = Order.create(user_id: user_id, item_id: item_id)
 
     address = Address.create(postal_code: postal_code, area_id: area_id, city: city,
                              house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
